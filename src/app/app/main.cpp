5009f0ef2feca9bd44e370a4f6d72455389d8a67
NeveykoArt <neveiko29@gmail.com>
2023-11-01 12:30:23 +0700

    3 commit

---
+++ src/app/app/main.cpp
@@ -0,0 +1,19 @@
+#include <llvmlib/callgraph.hpp>
+#include <llvmlib/defuse.hpp>
+
+#include <cxxopts.h>
+
+#include <fstream>
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/SourceMgr.h>
+
+int main(int argc, char **argv) {
+    
+    std::cout << "Hello, world!\n";
+    
+    return 0;
+}
